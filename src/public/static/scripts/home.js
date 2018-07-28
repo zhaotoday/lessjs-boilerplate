e37099fa54e695dd845164df2693b00eebadf109
zhaojintian <729234283@qq.com>
2018-07-28 12:52:24 +0800

    优化

---
+++ src/public/static/scripts/home.js
@@ -0,0 +1,1 @@
+!function(n){var r={};function t(o){if(r[o])return r[o].exports;var e=r[o]={exports:{},id:o,loaded:!1};return n[o].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}t.m=n,t.c=r,t.p="//yly-cdn.liruan.cn/",t(0)}({0:function(n,r,t){"use strict";t(24)},24:function(n,r){}});
