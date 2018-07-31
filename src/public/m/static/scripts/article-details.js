b5d6baa845385bd9e645a0cd4fcb473bfdf2300e
zhaojintian <729234283@qq.com>
2018-07-31 15:20:38 +0800

    优化

--- src/public/m/static/scripts/article-details.js
+++ src/public/m/static/scripts/article-details.js
@@ -1,1 +1,1 @@
-!function(r){var t={};function n(o){if(t[o])return t[o].exports;var e=t[o]={exports:{},id:o,loaded:!1};return r[o].call(e.exports,e,e.exports,n),e.loaded=!0,e.exports}n.m=r,n.c=t,n.p="//www.rjwb.cn/m/",n(0)}({0:function(r,t,n){"use strict";n(9)},9:function(r,t){}});
+!function(r){var t={};function n(o){if(t[o])return t[o].exports;var e=t[o]={exports:{},id:o,loaded:!1};return r[o].call(e.exports,e,e.exports,n),e.loaded=!0,e.exports}n.m=r,n.c=t,n.p="//www.rjwb.cn/",n(0)}({0:function(r,t,n){"use strict";n(8)},8:function(r,t){}});
